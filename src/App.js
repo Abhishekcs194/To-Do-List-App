08974434264564c5fc4d302ccff8a4f19f55da8b
Abhishekcs194 <88553674+Abhishekcs194@users.noreply.github.com>
2022-05-26 11:38:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
 
 import './App.css';
 
-<<<<<<< Updated upstream
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 import { useState } from "react"
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
@@ -30,7 +11,6 @@ function App() {
   return (
     <div className='togo-app'>
       <TodoList />
->>>>>>> Stashed changes
     </div>
   );
     
