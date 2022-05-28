951f2d7c2636407ab4e1be9cc6768cb0e33a7ce0
Abhishekcs194 <88553674+Abhishekcs194@users.noreply.github.com>
2022-05-28 15:09:33 +0530

    Revert "lint (#5)" (#7)
    This reverts commit dae31136f3792b81c6a7eaba8764b406046494dc.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-
+import logo from './logo.svg';
 import './App.css';
 
-import { useState } from "react"
-import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
-
 function App() {
-
-
   return (
-    <div className='togo-app'>
-      <TodoList />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-    
 }
 
 export default App;
