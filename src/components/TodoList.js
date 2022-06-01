46127b91545fff85846ea2512f236f4c95a479a4
Arjun Raj <rajarju@users.noreply.github.com>
2022-06-01 14:05:03 +0530

    add: api

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,71 +1,127 @@
-import React, { useEffect, useState } from 'react';
-import TodoForm from './TodoForm';
-import Todo from './Todo';
-
+import React, { useEffect, useState } from "react";
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+import axios from "axios";
 
 function TodoList() {
+  // Store the todos
   const [todos, setTodos] = useState([]);
 
+  // Flag to wait for loading
   const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(()=>{
-    const todosString = window.localStorage.todos;
-    
-    if(todosString){
-      const todoParse = JSON.parse(todosString);
-      
-      setTodos(todoParse);
+  // useEffect called once after the component is loaded
+  useEffect(() => {
+    loadTodos();
+  }, []);
+
+  /**
+   * Load todos and update list for ui
+   */
+  const loadTodos = async () => {
+    try {
+      const { data } = await axios.get("/api/todos");
+      setTodos(data);
+      setIsLoaded(true);
+    } catch (e) {
+      console.error(e);
     }
-    setIsLoaded(true);
-  },[])
+  };
 
-  useEffect(() =>{
-    console.log(todos,"saving useEffect");
-    if(isLoaded){window.localStorage.setItem('todos',JSON.stringify(todos))}
-    
-  })
-
-
-
-  const addTodo = todo => {
+  /**
+   * Add Todo
+   * @param {*} todo
+   * @returns
+   */
+  const addTodo = async (todo) => {
     if (!todo.text || /^\s*$/.test(todo.text)) {
       return;
     }
+    try {
+      // Axios POST data to api and get response object
+      const response = await axios.post("/api/todos", {
+        ...todo,
+      });
 
-    const newTodos = [todo, ...todos];
+      // Extract response data
+      const newTodo = response.data;
 
-    setTodos(newTodos);
-    console.log(...todos);
-
-
+      // Update ui list of todos
+      const newTodos = [newTodo, ...todos];
+      setTodos(newTodos);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  const updateTodo = (todoId, newValue) => {
+  /**
+   * Update a todo with id
+   * @param {*} todoId
+   * @param {*} newValue
+   * @returns
+   */
+  const updateTodo = async (todoId, newValue) => {
     if (!newValue.text || /^\s*$/.test(newValue.text)) {
       return;
     }
 
-    setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    try {
+      // Axios POST data to api and get response object
+      const response = await axios.put(`/api/todos/${todoId}`, {
+        text: newValue.text,
+      });
+
+      // Extract response data
+      const newTodo = response.data;
+
+      setTodos((prev) =>
+        prev.map((item) => (item._id === todoId ? newTodo : item))
+      );
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  const removeTodo = id => {
-    const removedArr = [...todos].filter(todo => todo.id !== id);
+  /**
+   * Remove todo item
+   * @param {*} _id
+   */
+  const removeTodo = async (_id) => {
+    try {
+      // Axios POST data to api and get response object
+      await axios.delete(`/api/todos/${_id}`);
 
-    setTodos(removedArr);
+      const removedArr = [...todos].filter((todo) => todo._id !== _id);
 
-    
-
+      setTodos(removedArr);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  const completeTodo = id => {
-    let updatedTodos = todos.map(todo => {
-      if (todo.id === id) {
+  /**
+   * Toggle todo status
+   * @param {*} id
+   */
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo._id === id) {
         todo.isComplete = !todo.isComplete;
       }
       return todo;
     });
     setTodos(updatedTodos);
   };
+
+  // Early return pattern
+  // Render loading
+  if (!isLoaded) {
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    );
+  }
 
   return (
     <>
