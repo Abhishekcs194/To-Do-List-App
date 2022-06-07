623f45e827367e2ea1f00366511247f4309ede36
teemul491 <72974912+teemul491@users.noreply.github.com>
2022-06-07 11:53:07 +0530

    login_frontend
    recieving error as it is not posting the request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
-import TodoList from "./components/TodoList";
+// import TodoList from "./components/TodoList";
 import "./App.css";
+//import Homepage from "./components/homepage/homepage"
+//import Login from "./components/login/login"
+import Register from "./components/register/register";
 
 function App() {
   return (
     <div className="todo-app">
-      <TodoList />
-    </div>
+      {/* <TodoList> */}
+      {      /*<Homepage /> */}
+      {/* <Login /> */}
+      <Register />
+    </div >
   );
 }
 
