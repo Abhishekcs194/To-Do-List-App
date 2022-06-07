a57c50383ab4b4c6c1294eb2516060e3d151d35f
Abhishekcs194 <88553674+Abhishekcs194@users.noreply.github.com>
2022-06-07 13:11:26 +0530

    debug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import TodoList from "./components/TodoList";
+import TodoList from "./components/TodoList";
 import "./App.css";
 //import Homepage from "./components/homepage/homepage"
 //import Login from "./components/login/login"
@@ -8,11 +8,11 @@ import Register from "./components/register/register";
 function App() {
   return (
     <div className="todo-app">
-      {/* <TodoList> */}
+      <TodoList>
       {      /*<Homepage /> */}
       {/* <Login /> */}
       <Register />
-    </div >
+    </div>
   );
 }
 
